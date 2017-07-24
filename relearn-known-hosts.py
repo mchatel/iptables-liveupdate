8079a2fadd489ef43bdf154961ae8dfd97584868
Marc Chatel <mchatel@mnubo.com>
2017-07-24 17:13:46 -0600

    Phase 1 of major iptables-liveupdate refactoring
    - Comply with "modern" Python coding style.
    - Document the coding style followed in CODING-RULES.txt.
    - Eliminate empty config file and create a proper sample config file.
    - Refactor module "util.py".
    - Refactor top-level script "relearn-known-hosts.py".
    - Refactor top-level script "iptables-backup.py".
    - Add IPv6 support to iptables-backup.py.
    - Do substantial re-test and code coverage of all three
      refactored Python source files.
    
    NOTE: For now, scripts iptables-liveupdate.py and
          iptables-liveupdate-driver.py are BROKEN,
          since they are not up to date yet with changes done
          in the new util.py

--- relearn-known-hosts.py
+++ relearn-known-hosts.py
@@ -3,75 +3,89 @@
 
 #
 # This file is subject to the terms and conditions defined in
-# file 'LICENSE', which is part of this source code package.
+# file "LICENSE", which is part of this source code package.
 #
 
 
 import logging
-import pprint
 import subprocess
 
-from util import read_hosts_config, config_file_name
+from util import parse_std_arguments, config_logger
+from util import add_user_host_part, read_hosts_config
 
-logger = logging.getLogger()
-pp = pprint.PrettyPrinter(indent=2)
 
-ssh_timeout_secs = 5
-
-# ------------------------------------ MAIN PROGRAM ------------------------------------ #
+# -------------------------- MAIN PROGRAM -------------------------- #
 if __name__ == "__main__":
+    cmd_arguments = parse_std_arguments(
+        """
+        Forget and re-learn destination SSH host keys.
+        Useful in environments where the network is trusted and hosts
+        are frequently re-created or re-installed, and there is no
+        tracking of SSH host keys. USING THIS SCRIPT WITHOUT UNDERSTANDING
+        AND CONTROLLING ITS SECURITY IMPLICATIONS BREAKS THE SSH
+        MAN-IN-THE-MIDDLE ATTACK DETECTION SYSTEM!
+        """)
 
-    hosts_config = read_hosts_config()
+    config_logger(cmd_arguments)
+    log = logging.getLogger()
 
-    logger.info('Removing known_hosts entries for all IPs in ...'.format(config_file_name))
+    hosts_config = read_hosts_config(cmd_arguments.cfgfile)
+
+    log.info("Removing known_hosts SSH keys for all hosts in %s ...",
+             cmd_arguments.cfgfile)
 
     clear_backup_cmd_str = "rm -f ~/.ssh/known_hosts.old"
 
     subprocess.call(clear_backup_cmd_str, shell=True)
 
-    for one_entry in hosts_config:
+    part0 = "ssh-keygen -q -R"
+    part2 = "> /dev/null 2>&1"
 
-        if "ip" in one_entry.keys():
-            one_ip = one_entry["ip"]
-            cmd_str = "ssh-keygen -q -R " + one_ip + " > /dev/null 2>&1"
+    for item in hosts_config:
+        for k in ["ip", "name"]:
+            if k in item.keys():
+                cmd_str = "{0} {1} {2}".format(part0, item[k], part2)
+                subprocess.call(cmd_str, shell=True)
+                subprocess.call(clear_backup_cmd_str, shell=True)
 
+    log.info("Removal complete.")
+    log.info(
+        "Re-learning known_hosts entries for all active host IPs ...")
+    log.debug("Setting SSH connection timeout to %d seconds.",
+              cmd_arguments.timeout)
+
+    for item in hosts_config:
+
+        num_missing = 0
+
+        for needed_field in ["ip", "name", "user", "state"]:
+            if needed_field not in item.keys():
+                log.warning(
+                    "A config file entry misses field '%s'.", needed_field)
+                num_missing += 1
+
+        if num_missing > 0:
+            log.warning("Skipping a config file entry that misses fields.")
+            continue
+
+        if item["state"].lower() != "on":
+            log.debug("Ignoring host '%s' not in ON state", item["name"])
+            continue
+
+        opt1 = "-o BatchMode=yes"
+        opt2 = "-o ConnectTimeout={0}".format(cmd_arguments.timeout)
+        opt3 = "-o StrictHostKeyChecking=no"
+        test_cmd = " echo -n"
+
+        base_cmd = "ssh {0} {1} {2} -q ".format(opt1, opt2, opt3)
+
+        for k in ["ip", "name"]:
+            cmd_str = "{0} {1}".format(
+                          add_user_host_part(
+                              base_cmd, item["user"], item[k]), test_cmd)
+
+            log.debug("Learning SSH host key (if possible) for '%s' ...",
+                      item[k])
             subprocess.call(cmd_str, shell=True)
             subprocess.call(clear_backup_cmd_str, shell=True)
 
-        if "name" in one_entry.keys():
-            one_name = one_entry["name"]
-            cmd_str = "ssh-keygen -q -R " + one_name + " > /dev/null 2>&1"
-
-            subprocess.call(cmd_str, shell=True)
-            subprocess.call(clear_backup_cmd_str, shell=True)
-
-    logging.info('Removal complete')
-    logging.info('Re-learning known_hosts entries for all active host IPs ...')
-
-    for one_entry in hosts_config:
-
-        if "ip" not in one_entry.keys():
-            continue
-        if "name" not in one_entry.keys():
-            continue
-        if "user" not in one_entry.keys():
-            continue
-        if "state" not in one_entry.keys():
-            continue
-
-        if one_entry["state"].lower() != "on":
-            continue
-
-        cmd_str = 'ssh  -o BatchMode=yes  -o ConnectTimeout={0} -o StrictHostKeyChecking=no -q '.format(
-            ssh_timeout_secs)
-
-        if one_entry["user"] != "SRCUSER":
-            cmd_str += one_entry["user"] + "@"
-
-        cmd_str1 = cmd_str + one_entry["ip"] + " hostname"
-        cmd_str2 = cmd_str + one_entry["name"] + " hostname"
-
-        subprocess.call(cmd_str1, shell=True)
-        subprocess.call(clear_backup_cmd_str, shell=True)
-        subprocess.call(cmd_str2, shell=True)
-        subprocess.call(clear_backup_cmd_str, shell=True)
